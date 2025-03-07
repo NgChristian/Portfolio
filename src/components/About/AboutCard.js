98b7a9b7ea3dd76695f40a0a860a28f7089d6128
Nguyen Christian <ngchristian93@gmail.com>
2025-03-07 17:11:08 +0100

    maj

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Christian NGUYEN </span>
+            from <span className="purple"> Noisy-le-Grand, France.</span>
             <br />
             I am currently employed as a software developer at Juspay.
             <br />
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Christian NGUYEN</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import podcastAudio from "../../Assets/Projects/podcast.mp4";
 
 function Projects() {
   return (
@@ -26,7 +26,6 @@ function Projects() {
               title="SAE_Analyse_ParcourSup"
               description="Projet d'analyse de données sur les candidatures ParcourSup pour le BUT Informatique, développé avec Python, Pandas, Matplotlib et Jupyter Notebook. Permet d'extraire des tendances, d'étudier les profils des candidats et de proposer un classement basé sur des critères de sélection clés."
               ghLink="https://github.com/NgChristian/SAE_Analyse_ParcourSup"
-              demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
@@ -37,7 +36,6 @@ function Projects() {
               title="SAE_Dev_Web"
               description="Application web de gestion de projet inspirée de Trello, développée avec HTML, CSS, TypeScript, React, Java Spring Boot et Node.js. Permet la création de tableaux, de listes et de cartes pour organiser les tâches efficacement."
               ghLink="https://github.com/NgChristian/SAE_Dev_Web"
-              demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
@@ -48,18 +46,17 @@ function Projects() {
               title="Miam-o-Pedia"
               description="Catalogue interactif de plats avec un sélecteur multimodal, développé avec HTML, CSS, JavaScript, Python et Flask. Recherche en temps réel, filtres par ordre alphabétique, gestion des favoris et création de catégories pour organiser les plats efficacement."
               ghLink="https://github.com/NgChristian/Miam-o-Pedia"
-              demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={editor}
               isBlog={false}
-              title="Podcast"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Podcast - L'IA va-t-elle remplacer les développeurs ?"
+              description="Un débat captivant sur l'impact de l'intelligence artificielle dans le monde du développement. 
+              Sommes-nous à l’aube d’une révolution où l’IA remplacera les programmeurs ? Quatre experts confrontent leurs opinions."
+              audio={podcastAudio}
             />
           </Col>
         </Row>
